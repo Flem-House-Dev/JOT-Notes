90fcad330b92f56919f7df24c009a3836b03e0cc
JFleming963 <144638343+JFleming963@users.noreply.github.com>
2024-09-08 21:28:32 -0500

    JN-24 moved config code to the config file

--- server/controllers/noteController.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect(
-  process.env.MONGODB_URI ||
-    "mongodb+srv://flemhousetech:tZJht7c0Xa6xDCIe@cluster0.z4lkeqv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-);
-
-module.exports = mongoose.connection;
