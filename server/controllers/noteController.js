5672b638e5d1bc161275bf10620adb3f1c0990c9
JFleming963 <144638343+JFleming963@users.noreply.github.com>
2024-09-28 16:30:58 -0500

    updated noteController file, and server package.json file

--- server/controllers/noteController.js
+++ server/controllers/noteController.js
@@ -1,11 +1,3 @@
-const mongoose = require("mongoose");
-
-mongoose.connect(
-  process.env.MONGODB_URI ||
-    "mongodb+srv://flemhousetech:tZJht7c0Xa6xDCIe@cluster0.z4lkeqv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-);
-
-module.exports = mongoose.connection;
 const Note = require("../models/noteModel");
 
 // Get all notes
