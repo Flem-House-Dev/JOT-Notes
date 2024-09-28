c91e3912d1648840ce3f13250f3d1b41df234b6e
JFleming963 <144638343+JFleming963@users.noreply.github.com>
2024-09-28 15:22:24 -0500

    updated vite.config.js to have proxy server and sourcemap. Also, updated .gitignore

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,20 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
   plugins: [react()],
   server: {
     port: 3000,
     open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '/api')
+      },
+    }
   },
 });
