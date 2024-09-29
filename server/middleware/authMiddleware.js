947d04529eab33645da667d452240130bc2ed00b
JFleming963 <144638343+JFleming963@users.noreply.github.com>
2024-09-28 20:42:27 -0500

    removed console.log's

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -12,8 +12,6 @@ const protect = async (req, res, next) => {
       token = req.headers.authorization.split(" ")[1].trim();
       const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
-      console.log('Received token:', token);
-
       req.user = await User.findById(decoded.id).select("-password");
       next();
     } catch (error) {
